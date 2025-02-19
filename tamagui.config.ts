9974909e67dc55a37dc0196ca32a25a8f9de6b50
Saahil <neon@saahild.com>
2025-02-19 11:07:18 -0500

    fix:change template (#2)

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,12 +1,12 @@
-import { defaultConfig } from "@tamagui/config/v4";
-import { createTamagui } from "tamagui";
+import { config } from '@tamagui/config/v3'
+import { createTamagui } from 'tamagui'
 
-export const config = createTamagui(defaultConfig);
+export const tamaguiConfig = createTamagui(config)
 
-export default config;
+export default tamaguiConfig
 
-export type Conf = typeof config;
+export type Conf = typeof tamaguiConfig
 
-declare module "tamagui" {
+declare module 'tamagui' {
   interface TamaguiCustomConfig extends Conf {}
 }
